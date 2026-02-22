08e8e1ce4f9ed23b2246edd76bb733a1a7c8d428
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2026-02-22 09:35:36 +0100

    feat: POST /upload

--- go-mariadb-benchmark/internal/dto/request/upload.go
+++ go-mariadb-benchmark/internal/dto/request/upload.go
@@ -3,5 +3,5 @@ package request
 import "mime/multipart"
 
 type Upload struct {
-	File *multipart.FileHeader `json:"file"`
+	File *multipart.FileHeader `form:"file"`
 }
--- go-mariadb-benchmark/internal/service/upload/upload.go
+++ go-mariadb-benchmark/internal/service/upload/upload.go
@@ -2,8 +2,11 @@ package upload
 
 import (
 	"context"
+	"errors"
+	"io"
 
 	"github.com/Ascension-EIP/benchmark/go-mariadb-benchmark/internal/dto/request"
+	"github.com/Ascension-EIP/benchmark/go-mariadb-benchmark/internal/model"
 	"github.com/Ascension-EIP/benchmark/go-mariadb-benchmark/internal/repo"
 )
 
@@ -16,5 +19,26 @@ func New(r repo.Upload) *Service {
 }
 
 func (s *Service) Upload(c context.Context, userId uint, req request.Upload) error {
+	if req.File == nil {
+		return errors.New("missing file")
+	}
+	file, err := req.File.Open()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	upload := model.Upload{
+		UserID: userId,
+		File:   data,
+	}
+	if err := s.r.AddFile(&upload); err != nil {
+		return err
+	}
+
 	return nil
 }
